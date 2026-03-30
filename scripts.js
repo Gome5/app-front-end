0d3d79936c807e4bd3ef299d2a826ff0a5c549ca
Gabriel <gabrielfaria@gea.inatel.br>
2026-03-29 21:39:17 -0300

    feat: atualizações funções get e visual exclusão

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
 let receitas = [];
 let despesas = [];
 
+function limparCamposReceita() {
+  document.getElementById("descReceita").value = "";
+  document.getElementById("valorReceita").value = "";
+  document.getElementById("dataReceita").value = "";
+}
+
+function limparCamposDespesa() {
+  document.getElementById("descDespesa").value = "";
+  document.getElementById("valorDespesa").value = "";
+  document.getElementById("dataDespesa").value = "";
+}
+
 /*
   --------------------------------------------------------------------------------------
   Função para obter a lista existente do servidor via requisição GET
@@ -21,6 +33,7 @@ const getReceitas = async () => {
     .then((data) => {
       data.receitas.forEach(item => {
         receitas.push({
+          id: item.id,
           desc: item.descricao,
           valor: parseFloat(item.valor),
           data: item.data_entrada
@@ -35,6 +48,41 @@ const getReceitas = async () => {
 
 getReceitas();
 
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter a lista existente do servidor via requisição GET despesas
+  --------------------------------------------------------------------------------------
+*/
+const getDespesas = async () => {
+  let url = 'http://192.168.1.112:5000/despesas';
+
+  fetch(url, {
+    method: 'get',
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.despesas.forEach(item => {
+        despesas.push({
+          id: item.id,
+          desc: item.descricao,
+          valor: parseFloat(item.valor),
+          data: item.data_entrada
+        });
+      });
+      atualizarTela();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+getDespesas();
+
 function addReceita() {
   const desc = document.getElementById("descReceita").value;
   const valor = parseFloat(document.getElementById("valorReceita").value);
@@ -46,7 +94,7 @@ function addReceita() {
   }
 
   limparCamposReceita();
-  postItem(desc, valor, data);
+  postItem(desc, valor, data, "receita");
 }
 
 function addDespesa() {
@@ -59,21 +107,8 @@ function addDespesa() {
     return;
   }
 
-  despesas.push({ desc, valor, data });
   limparCamposDespesa();
-  atualizarTela();
-}
-
-function limparCamposReceita() {
-  document.getElementById("descReceita").value = "";
-  document.getElementById("valorReceita").value = "";
-  document.getElementById("dataReceita").value = "";
-}
-
-function limparCamposDespesa() {
-  document.getElementById("descDespesa").value = "";
-  document.getElementById("valorDespesa").value = "";
-  document.getElementById("dataDespesa").value = "";
+  postItem(desc, valor, data, "despesa");
 }
 
 function atualizarTela() {
@@ -92,6 +127,9 @@ function atualizarTela() {
         <td>${r.desc}</td>
         <td>R$ ${r.valor.toFixed(2)}</td>
         <td>${r.data}</td>
+        <td>
+          <img class="btn-excluir" src="https://cdn-icons-png.flaticon.com/512/126/126468.png" onClick="deletarItem(${d.id}, 'despesa')">
+        </td>
       </tr>
     `;
   });
@@ -105,6 +143,9 @@ function atualizarTela() {
         <td>${d.desc}</td>
         <td>R$ ${d.valor.toFixed(2)}</td>
         <td>${d.data}</td>
+        <td>
+          <img class="btn-excluir" src="https://cdn-icons-png.flaticon.com/512/126/126468.png" onClick="deletarItem(${d.id}, 'despesa')">
+        </td>
       </tr>
     `;
   });
@@ -134,14 +175,19 @@ function filtrarLista() {
   Função para colocar um item na lista do servidor via requisição POST
   --------------------------------------------------------------------------------------
 */
-const postItem = async (inputDesc, inputValue, inputDate) => {
+const postItem = async (inputDesc, inputValue, inputDate, type) => {
   const formData = new FormData();
   formData.append('descricao', inputDesc);
   formData.append('valor', inputValue);
   formData.append('data', inputDate);
 
   console.log('FormData:', formData.get('descricao'), formData.get('valor'), formData.get('data'));
-  let url = 'http://192.168.1.112:5000/receita';
+  let url = '';
+  if(type === "receita") {
+    url = 'http://192.168.1.112:5000/receita';
+  } else if(type === "despesa") {
+    url = 'http://192.168.1.112:5000/despesa';
+  }
   fetch(url, {
     method: 'post',
     body: formData
@@ -150,9 +196,42 @@ const postItem = async (inputDesc, inputValue, inputDate) => {
       if(!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
       }
-      console.log(response.json());
+      if(type === "receita") {
+        getReceitas();
+      } else if(type === "despesa") {
+        getDespesas();
+      }
     })
     .catch((error) => {
       console.error('Error:', error);
+      alert('Erro ao adicionar item. Verifique os dados ou tente novamente.');
     });
 }
+
+const deletarItem = (id, tipo) => {
+  if (!confirm('Tem certeza que deseja excluir?')) return;
+
+  let url = '';
+
+  if (tipo === 'receita') {
+    url = `http://192.168.1.112:5000/receita?id=${id}`;
+  } else {
+    url = `http://192.168.1.112:5000/despesa?id=${id}`;
+  }
+
+  fetch(url, {
+    method: 'DELETE'
+  })
+    .then(response => {
+      if (!response.ok) throw new Error('Erro ao deletar');
+
+      alert('Item excluído com sucesso!');
+
+      getReceitas();
+      getDespesas();
+    })
+    .catch(error => {
+      console.error(error);
+      alert('Erro ao excluir item');
+    });
+};
